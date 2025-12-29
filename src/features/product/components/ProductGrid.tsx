6eafcbdc80d9e6c5b8facfb655480f32eded8871
Ali Abodaraa <abodaraaali50@gmail.com>
2025-12-29 11:31:16 +0200

    feat: implement pagination for products

--- src/features/product/components/ProductGrid.tsx
+++ src/features/product/components/ProductGrid.tsx
@@ -1,7 +1,9 @@
 import { ParsedSearchParams } from "@/features/product/search-params";
-import ProductCard from "./ProductCard";
 import { getProducts } from "@/features/product/queries/get-products";
 import NoProductFound from "@/features/product/components/NoProductFound";
+import { ProductsWithMetadata } from "../types";
+import { ProductsPagination } from "./ProductsPagination";
+import ProductCard from "./ProductCard";
 
 interface ProductGridProps {
   searchParams: ParsedSearchParams;
@@ -12,7 +14,19 @@ export default async function ProductGrid({ searchParams }: ProductGridProps) {
   if (products.length === 0) {
     return <NoProductFound />;
   }
+  const count = products.length;
+  const skip = searchParams.size * searchParams.page;
+  const take = Math.min(searchParams.size, 100);
+  const hasNextPage = count > skip + take;
+  const currentPage = searchParams.page;
 
+  const ProductsMetadata: ProductsWithMetadata["metadata"] = {
+    count,
+    hasNextPage,
+    totalPages: Math.ceil(count / searchParams.size),
+    currentPage,
+  };
+  const paginatedProducts = products.slice(skip, skip + take);
   return (
     <>
       <div className="mb-4">
@@ -20,12 +34,14 @@ export default async function ProductGrid({ searchParams }: ProductGridProps) {
           Showing {products.length} product{products.length !== 1 ? "s" : ""}
         </p>
       </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {products.map((product) => (
+        {paginatedProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      Pagination
+
+      <ProductsPagination paginatedProductsMetadata={ProductsMetadata} />
     </>
   );
 }
